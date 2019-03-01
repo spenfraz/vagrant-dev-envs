bce8aaf5a2704f5f9c49bd37850db0c648dc7400
Spencer Frazier <spenfraz@gmail.com>
2019-02-28 23:20:44 -0600

    install wget & mysql, mysql start & set root pwd

--- cent_flask-note-api_dev/flask-env.sh
+++ cent_flask-note-api_dev/flask-env.sh
@@ -11,3 +11,15 @@ sudo yum -y install python36u
 
 sudo yum -y install python36u-pip
 sudo yum -y install python36u-devel
+
+sudo yum -y install wget
+
+sudo wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
+sudo yum -y update
+
+sudo yum -y install mysql-server
+sudo systemctl start mysqld
+
+sudo mysqladmin -u root password root123
+
