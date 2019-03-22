5f090e57001b61cd071a54fe34fc009ef328d174
unknown <spenfraz@gmail.com>
2019-03-22 18:57:12 -0500

    install java 8, maven, git and chown cloned project files

--- cent_schedule-scraper-soup_dev/run.sh
+++ cent_schedule-scraper-soup_dev/run.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+echo "===== Update yum ====="
+sudo yum -y update >> /home/vagrant/upstart-log.txt 2>&1
 
+echo "===== Install OpenJDK Java 8 ====="
+sudo yum -y install java-1.8.0-openjdk-devel >> /home/vagrant/upstart-log.txt 2>&1
+
+echo "===== Install Maven ====="
+sudo yum -y install maven >> /home/vagrant/upstart-log.txt 2>&1
+
+echo "===== Install Git ====="
+sudo yum -y install git >> /home/vagrant/upstart-log.txt 2>&1
+
+echo "===== Clone schedule-scraper-soup ====="
+git clone https://github.com/spenfraz/schedule-scraper-soup.git >> /home/vagrant/upstart-log.txt 2>&1
+
+echo "===== chown all project files (vagrant:vagrant) ===="
+sudo chown vagrant:vagrant /home/vagrant/schedule-scraper-soup/ -Rh
