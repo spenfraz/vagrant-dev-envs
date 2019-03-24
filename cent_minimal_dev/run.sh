79327333d602aff53f5c7779a86ae98f5ed295f7
spenfraz <spenfraz@gmail.com>
2019-03-24 03:36:12 -0500

    match cent_minimal_dev on ansible_local branch

--- cent_minimal_dev/run.sh
+++ cent_minimal_dev/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "===== yum update ====="
+sudo yum -y update >> /home/vagrant/upstart-log.txt 2>&1
 
+echo "===== install git ====="
+sudo yum -y install git >> /home/vagrant/upstart-log.txt 2>&1
